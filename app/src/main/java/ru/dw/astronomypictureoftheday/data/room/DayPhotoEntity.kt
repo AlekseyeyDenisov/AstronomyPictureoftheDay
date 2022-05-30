875bb039c31dd2cad5ef976be467fc7e1601774d
77den <77denisov@gmail.com>
2022-05-30 11:16:30 +0300

    Дорабил DetailsFragment.kt

--- app/src/main/java/ru/dw/astronomypictureoftheday/data/room/DayPhotoEntity.kt
+++ app/src/main/java/ru/dw/astronomypictureoftheday/data/room/DayPhotoEntity.kt
@@ -2,6 +2,7 @@ package ru.dw.astronomypictureoftheday.data.room
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import ru.dw.astronomypictureoftheday.utils.EMPTY_STRING
 
 
 @Entity(tableName = "nasa_table")
@@ -10,9 +11,9 @@ data class DayPhotoEntity(
     @PrimaryKey(autoGenerate = true)
     val id:Long,
     val date: String,
-    val hdUrl: String,
-    val explanation: String,
-    val title: String,
-    val url: String,
-    val mediaType:String
+    val hdUrl: String = EMPTY_STRING,
+    val explanation: String = EMPTY_STRING,
+    val title: String = EMPTY_STRING,
+    val url: String =EMPTY_STRING,
+    val mediaType:String = EMPTY_STRING
 )
